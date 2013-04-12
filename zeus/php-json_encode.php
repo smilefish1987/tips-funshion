04bc286ec71a00eec95b027c3c09c4a07cc1fd29
smilefish <smilefish1987@gmail.com>
2013-04-12 12:38:07 +0800

    add tips content

--- zeus/php-json_encode.php
+++ zeus/php-json_encode.php
@@ -1,6 +1,6 @@
 <?php
 
-	$cate = array('china','america','america','janpa');
+	$cate = array('china','america','america','japan');
 	
 	//排重
 	$unique_cate = array_unique($cate);
@@ -8,6 +8,10 @@
 	echo json_encode($unique_cate);
 	echo json_encode(array_values($unique_cate));
 	
+	//输出结果如下
+	{"0":"china","1":"america","3":"japan"}
+	["china","america","japan"]
+	
+	//一个不规则的数组返回的是object，一个规则的数组返回了array 
 	//最后两条语句数据的数据是不一样，一个是object,一个是array
-    //测试github	
 ?>
